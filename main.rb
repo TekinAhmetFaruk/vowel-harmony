175dad3d83293d7fff0878057abd0c954c56e8d8
TekinAhmetFaruk <151140419+TekinAhmetFaruk@users.noreply.github.com>
2023-12-26 16:20:31 +0300

    main.rb

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ def is_an_exception?(word_to_search)
   # BUU' na istisna kapsaminde uygun olan kelimeler listesidir. Degistirmeyiniz!
   exception_words = %w[anne dahi elma hangi hani inanmak]
 
-  == WRITE CODE ==
+  exception_words.include?(word_to_search)
 end
 
 # BUU aranmayan kelimeleri kontrol eden metot
@@ -14,7 +14,7 @@ def is_not_required?(word_to_search)
 
   # BUU aranmayan kelimeler listesidir. Degistirmeyiniz!
   not_required_words = %w[ahenk badem ceylan fidan gazete hamsi kestane limon model otomatik pehlivan selam tiyatro viraj ziyaret]
-  == WRITE CODE ==
+    not_required_words.include?(word_to_search)
 end
 
 # Bir kelimenin BUU' na uygun olup/olmadigini bildiren metot
@@ -24,10 +24,33 @@ def is_harmonic?(word_to_search)
   thick_vowels = "aıou"
   thin_vowels = "eiöü"
 
-  == WRITE CODE ==
+  ince_harf = false
+  kalın_harf = false
+  
+  word_to_search.chars.each do |harf|
+   kalın_harf = true if thick_vowels.include?(harf)
+   ince_harf = true if thin_vowels.include?(harf)
+  end
+
+   kalın_harf && ince_harf
+
 end
 
 
  words = gets.chomp.split # Kelimeleri al
 
-  == WRITE CODE ==
+ words.each do |word|
+  if is_an_exception?(word)
+      puts "#{word} : ISTSN"
+      word = ""
+  
+  elsif is_not_required?(word)
+      puts "#{word} : ARNMZ"
+      word = ""
+  
+  elsif is_harmonic?(word)
+      puts "#{word} : UYGNDGL"
+  else
+      puts "#{word} : UYGN"
+ end
+end
