7780252182e561fe0ef18c4a73a1da0a04895d64
ismailhakkituran <147559619+ismailhakkituran@users.noreply.github.com>
2023-12-26 15:09:17 +0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# BUU' na istisna kapsaminde uygun olan kelimeleri kontrol eden metot
+def is_an_exception?(word_to_search)
+
+  # BUU' na istisna kapsaminde uygun olan kelimeler listesidir. Degistirmeyiniz!
+  exception_words = %w[anne dahi elma hangi hani inanmak]
+
+  == WRITE CODE ==
+end
+
+# BUU aranmayan kelimeleri kontrol eden metot
+def is_not_required?(word_to_search)
+
+  # BUU aranmayan kelimeler listesidir. Degistirmeyiniz!
+  not_required_words = %w[ahenk badem ceylan fidan gazete hamsi kestane limon model otomatik pehlivan selam tiyatro viraj ziyaret]
+  == WRITE CODE ==
+end
+
+# Bir kelimenin BUU' na uygun olup/olmadigini bildiren metot
+def is_harmonic?(word_to_search)
+
+  #Kontrol icin kullanilacak kalin ve ince harfler
+  thick_vowels = "aıou"
+  thin_vowels = "eiöü"
+
+  == WRITE CODE ==
+end
+
+
+ words = gets.chomp.split # Kelimeleri al
+
+  == WRITE CODE ==
